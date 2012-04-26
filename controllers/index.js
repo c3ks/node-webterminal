a202b452321c654847cde1c3a60e81a0f08cb48b
Sascha Ohms <sascha@534f.de>
2012-04-27 00:21:59 +0200

    folders ...

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+var exec = require('child_process').exec;
+var child;
+
+exports.mypty = function(input, callback) {
+
+  child = exec(input, function (error, stdout, stderr) {
+    if (error === null)
+      callback(stdout);
+  });
+};
