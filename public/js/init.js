47ed6f732bd155b454ded495d162fe1afdb3f60a
Philipp Hirsch <philipp-hirsch@gmx.net>
2012-04-26 21:44:36 +0200

    First Javascript functions

---
+++ public/js/init.js
@@ -0,0 +1,16 @@
+$(document).ready(init);
+
+function init()
+{
+	$("body").keypress(submitInput);
+	$("#cursor").focus();
+}
+
+
+function submitInput(event)
+{
+	if (event.which == 13)
+	{
+		alert ("Submitted");
+	}
+}
