29b4e9e9ab202f0993c2620efc9d5d642337ded2
Philipp Hirsch <philipp-hirsch@gmx.net>
2012-04-26 22:13:48 +0200

    Input will handled

--- public/js/init.js
+++ public/js/init.js
@@ -6,15 +6,25 @@ function init()
 	$("#cursor").focus();
 }
 
-
 function submitInput(event)
 {
 	if (event.which == 13)
 	{
-		alert("Submitted");
 		var input = $("#cursor").val();
 		$("#cursor").val("");
 
+		appendRequest(input);
 		IoWrapper.send(input);
 	}
+}
+
+function appendRequest(req)
+{
+	$("#output").append(req);
+	$("#output").append("<br />");
+}
+
+function appendResponse(res)
+{
+	$("#output").append(res);
 }
