e1867bd373c2bbcd405fb3881f546dab3a0d1cd9
Enno Boland (Gottox) <g@s01.de>
2012-04-28 13:23:01 +0200

    Dropping express, using connect direct instead.

--- app.js
+++ app.js
@@ -2,44 +2,17 @@
  * Module dependencies.
  */
 
-var express = require('express')
+var connect = require('connect')
   , controllers = require('./controllers')
   , routes = require('./routes');
 
-var app = module.exports = express.createServer()
+var app = connect()
   , io = require('socket.io').listen(app);
 
 // Configuration
 
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+var app = connect()
+	.use(connect.logger('dev'))
+	.use(connect.static('public'))
+	.listen(3000);
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-io.sockets.on('connection', function (socket) {
-  socket.on('send', function (data) {
-    var input = data.data;
-    controllers.mypty(input, function(output) {
-      socket.emit('receive', {data: output});
-    });
-  });
-});
-
-app.listen(3000);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
